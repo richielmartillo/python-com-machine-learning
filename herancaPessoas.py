2db4fa8021b6234471a3c99790aaa71770283d1d
Richard Marlon Balestrim <richardbalestrim8@gemail.com>
2026-03-15 15:21:14 -0300

    Herançã pessoas

---
+++ herancaPessoas.py
@@ -0,0 +1,37 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def apresentar(self):
+        print(f'Olá, meu nome é {self.nome} e tenho {self.idade} anos de idade')
+
+
+class Funcionario(Pessoa):
+    def __init__(self, nome, idade, cargo):
+        super().__init__(nome, idade)
+        self.cargo = cargo
+
+    def trabalhar(self):
+        print(f'{self.nome} está trabalhando como {self.cargo}.')
+
+
+class Cliente(Pessoa):
+    def __init__(self, nome, idade, saldo):
+        super().__init__(nome, idade)
+        self.saldo = saldo
+
+    def comprar(self, valor_compra):
+        if valor_compra <= self.saldo:
+            self.saldo -= valor_compra
+            print(f'A sua compra de {valor_compra} foi aprovada! Seu saldo atual é de: R${self.saldo}')
+        else:
+            print('Saldo insuficiente')
+
+
+f1 = Funcionario('Maria', 38, 'Gerente de contas')
+#f1.trabalhar()
+
+c1 = Cliente('Arthur', 16, 200)
+#c1.apresentar()
+c1.comprar(300)
